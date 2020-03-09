199ed77fea185de9fad52e1f7a7306d3d364155c
Xavier Elon Hollingsworth <Xavierh93@hotmail.com>
2020-03-09 18:06:00 -0400

    Create combination_sum.java

---
+++ Medium/combination_sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null)
+            return null;
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if(target == 0)
+            return result;
+        
+        findCombinations(candidates, 0, target, new ArrayList<Integer>(), result);
+        
+        return result;
+    }
+    
+    public void findCombinations(int[] candidates, int index, int target, List<Integer> current, List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<Integer>(current));
+            return;
+        }
+        
+        for(int i = index; i < candidates.length; i++) {
+            if (candidates[i] <= target) {
+                current.add(candidates[i]);
+                findCombinations(candidates, i, target-candidates[i], current, result);
+                current.remove(new Integer(candidates[i]));
+            }
+        }
+        
+    }
+}
