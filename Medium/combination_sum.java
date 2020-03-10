a9b8ec205e86829cbebfd75ef606684657acaf04
Xavier Elon Hollingsworth <Xavierh93@hotmail.com>
2020-03-10 10:57:24 -0400

    Update combination_sum.java

--- Medium/combination_sum.java
+++ Medium/combination_sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        if (candidates == null)
+        if (candidates == null) {
             return null;
+        }
+        
         List<List<Integer>> result = new ArrayList<>();
         
-        if(target == 0)
-            return result;
-        
-        findCombinations(candidates, 0, target, new ArrayList<Integer>(), result);
+        findCombinations(candidates, 0, target, new ArrayList<>(), result);
         
         return result;
     }
     
     public void findCombinations(int[] candidates, int index, int target, List<Integer> current, List<List<Integer>> result) {
         if (target == 0) {
-            result.add(new ArrayList<Integer>(current));
+            result.add(new ArrayList<>(current));
             return;
         }
         
-        for(int i = index; i < candidates.length; i++) {
+        for (int i = index; i < candidates.length; i++) {
             if (candidates[i] <= target) {
                 current.add(candidates[i]);
                 findCombinations(candidates, i, target-candidates[i], current, result);
                 current.remove(new Integer(candidates[i]));
             }
         }
-        
     }
 }
